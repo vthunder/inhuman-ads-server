1f2fbbf3a91bb8f27c86c130b9a792ae36db1512
Dan Mills <danmills@sandmill.org>
2017-09-17 14:53:03 -0700

    add a couple of shortcodes to allow chunks of pages to be used only when the browser is Firefox / something else

--- wp-content/themes/inhuman/functions/shortcodes.php
+++ wp-content/themes/inhuman/functions/shortcodes.php
@@ -10,4 +10,22 @@
     }
   }
   add_shortcode('inhuman_install_button', 'inhuman_install_button');
+
+  function inhuman_ua_fx($atts, $content = null) {
+    if (strlen(strstr($_SERVER['HTTP_USER_AGENT'], "Firefox")) > 0) {
+      return $content;
+    } else {
+      return '';
+    }
+  }
+  add_shortcode('ua_fx', 'inhuman_ua_fx');
+
+  function inhuman_ua_other($atts, $content = null) {
+    if (strlen(strstr($_SERVER['HTTP_USER_AGENT'], "Firefox")) <= 0) {
+      return $content;
+    } else {
+      return '';
+    }
+  }
+  add_shortcode('ua_other', 'inhuman_ua_other');
 ?>
