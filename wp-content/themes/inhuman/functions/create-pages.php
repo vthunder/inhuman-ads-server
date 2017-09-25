c55ec2642521491961870008fb192e9d8682d93b
Dan Mills <danmills@sandmill.org>
2017-09-25 15:16:33 -0700

    Unconditionally update default pages when activating theme.

--- wp-content/themes/inhuman/functions/create-pages.php
+++ wp-content/themes/inhuman/functions/create-pages.php
@@ -1,28 +1,25 @@
 <?php
-  // programmatically create some basic pages, and then set Home and Blog
-  // setup a function to check if these pages exist
-  function the_slug_exists($post_name) {
-    global $wpdb;
-    if($wpdb->get_row("SELECT post_name FROM wp_posts WHERE post_name = '" . $post_name . "'", 'ARRAY_A')) {
-      return true;
+  // Creates a page, or updates it if it already exists
+  function create_or_update_page($title, $slug, $order, $content) {
+    $page_by_title = get_page_by_title($title);
+    $page_by_slug = get_page_by_path($page_path);
+    $page = array(
+      'post_type' => 'page',
+      'post_title' => $title,
+      'post_content' => $content,
+      'post_status' => 'publish',
+      'post_author' => 1,
+      'menu_order' => $order,
+      'post_slug' => $slug
+    );
+    if (isset($page_by_title->ID)) {
+      $page['ID'] = $page_by_title->ID;
+      wp_update_post($page);
+    } elseif (isset($page_by_slug->ID)) {
+      $page['ID'] = $page_by_slug->ID;
+      wp_update_post($page);
     } else {
-      return false;
-    }
-  }
-
-  // Creates a page if it does not yet exist
-  function create_page($title, $slug, $order, $content) {
-    $page = get_page_by_title($title);
-    if(!isset($page->ID) && !the_slug_exists($slug)){
-      wp_insert_post(array(
-        'post_type' => 'page',
-        'post_title' => $title,
-        'post_content' => $content,
-        'post_status' => 'publish',
-        'post_author' => 1,
-        'menu_order' => $order,
-        'post_slug' => $slug
-      ));
+      wp_insert_post($page);
     }
   }
 
@@ -48,15 +45,15 @@
     // '$page_content'. Each include re-defines the same variable.
 
     require_once(plugin_dir_path(__FILE__) . '../default_pages/about.php');
-    create_page('About', 'about', 0, $page_content);
+    create_or_update_page('About', 'about', 0, $page_content);
 
     require_once(plugin_dir_path(__FILE__) . '../default_pages/contribute.php');
-    create_page('Contribute', 'contribute', 1, $page_content);
+    create_or_update_page('Contribute', 'contribute', 1, $page_content);
 
-    create_page('High Scores', 'high-scores', 2, '');
-    create_page('Blog', 'blog', 3, '');
+    create_or_update_page('High Scores', 'high-scores', 2, '');
+    create_or_update_page('Blog', 'blog', 3, '');
 
     require_once(plugin_dir_path(__FILE__) . '../default_pages/subscribe.php');
-    create_page('Subscribe', 'subscribe', 4, $page_content);
+    create_or_update_page('Subscribe', 'subscribe', 4, $page_content);
   }
 ?>
