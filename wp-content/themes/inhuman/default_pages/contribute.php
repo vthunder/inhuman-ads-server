1c2bda0031d15f000a50f21afd76df70a563a3d0
Dan Mills <danmills@sandmill.org>
2017-09-25 15:17:26 -0700

    A few additional tweaks to contribute page: missing links, FAQ styling

--- wp-content/themes/inhuman/default_pages/contribute.php
+++ wp-content/themes/inhuman/default_pages/contribute.php
@@ -34,15 +34,15 @@
 
 <h3 class="faq-header">FAQ</h3>
 
-<dl>
+<dl class="install-faq">
   <dt>Can I upload screenshots that I have saved on my computer?</dt>
   <dd>Although we’d love to see the bizarre ads you’ve found in your journeys, inhuman ads can only come from Firefox Screenshots. That enables us to nab the URL where your ad came from and hold the publisher (i.e., the website where you found the ad) accountable.</dd>
   
   <dt>What is Firefox Screenshots?</dt>
-  <dd>It’s a handy tool for taking screenshots of your window, screen, or a smaller selection from within Firefox. Just look for that little scissor cutting on a dotted line in your toolbar or in the three dots of your web address field. Learn more</dd>
+  <dd>It’s a handy tool for taking screenshots of your window, screen, or a smaller selection from within Firefox. Just look for that little scissor cutting on a dotted line in your toolbar or in the three dots of your web address field. <a href="https://screenshots.firefox.com/">Learn more</a></dd>
   
   <dt>I am using Firefox Screenshots...but where’s the “Send to Inhuman Ads” button?</dt>
-  <dd>Ah-ha, you’re almost there! Just install the Inhuman Ads add-on and the next time you use Firefox Screenshots, the button will be there.</dd>
+  <dd>Ah-ha, you’re almost there! Just <a href="https://addons.mozilla.org/en-US/firefox/addon/inhuman-ads/">install the Inhuman Ads add-on</a> and the next time you use Firefox Screenshots, the button will be there.</dd>
   
   <dt>Why does Mozilla hate ads?</dt>
   <dd>We 💜 good, creative ads that reach the right users and make sense in context. It’s the dark side — poorly placed ads that hurt our heads and eyes — that we’re trying to call out. Ad blockers don’t solve this problem, either. Indiscriminately blocking ads hurts the writers, visual artists, and other creato  rs that depend on ad revenue to fund their work. You, the consumer, deserve better ads.</dd>
