281134b6d6f20dafb7cb26241ed0d0f3bf0f1bd3
Dan Mills <danmills@sandmill.org>
2017-10-25 13:28:56 -0700

    Remove account creation markup from screenshot edit page, this happens in page-login.php now.

--- wp-content/themes/inhuman/single-inhuman_screenshot.php
+++ wp-content/themes/inhuman/single-inhuman_screenshot.php
@@ -34,8 +34,9 @@
 <div class="content">
   <div class="left-bar"></div>
   <div class="center-content">
-  <?php if($edit && $isowner): ?>
-    <?php if (get_user_meta(get_current_user_id(), "inhuman_user_complete", true)): ?>
+
+    <?php if($edit && $isowner): ?>
+
       <div class="screenshot-edit">
         <div class="screenshot-card">
           <?php the_post_thumbnail('full'); ?>
@@ -53,121 +54,93 @@
           <input id="delete-screenshot" type="submit" class="button button-raised" value="Delete Post" />
         </form>
       </div>
+
     <?php else: ?>
-      <p>Welcome to Inhuman Ads and congrats on your first post! 🎉</p>
-      <p>Create a username below or continue as an Anonymous User, no password required. Whether you choose a username or opt for the default, Anonymous User, that’s the name that will appear in the leaderboard if the ads you post are upvoted. Share your email for account recovery, important updates, and to get responses to your comments.</p>
-      <div id="user-setup">
-        <span id="user-setup-error"></span>
-        <form id="user-setup-create">
-          <div>
-            <label for="name">Name:</label>
-            <input type="text" name="name" value="<?php echo user()->display_name; ?>">
-          </div>
-          <div>
-            <label for="email">Email:</label>
-            <input type="text" name="email" placeholder="(optional)">
-          </div>
-          <input type="button" class="button button-primary" value="Submit">
-        </form>
-        <form id="user-setup-existing">
-          <!--
-               <div>
-               <label for="email">Email:</label>
-               <input type="text" name="email" placeholder="">
-               </div>
-               <input type="button" class="button button-primary" value="Submit"> -->
-          This feature isn't implemented yet.<br>Create a new username on this browser/device.
-        </form>
-        <!--         <a id="existing-account-link" href="#">Use existing account</a> -->
-        <a id="create-account-link" href="#">Create account</a>
-      </div>
-      <p>Your comments and posts will be live on Inhuman Ads unless you delete them. If you’re logged in, you can click on any post that belongs to you to edit or delete it.</p>
-      <p class="tos-privacy">Use of Inhuman Ads is subject to these <a href="https://www.mozilla.org/en-US/about/legal/terms/mozilla/">terms and conditions</a> | Read our <a href="https://www.mozilla.org/en-US/privacy/websites/">Privacy Notice</a></p>
-    <?php endif; ?>
 
-  <?php else: ?>
-    <div class="screenshot-card">
-      <?php $prev_next = prevNextIds(get_the_ID(), ""); // FIXME: need to pass in $query_type here ?>
-      <?php if (!empty($prev_next['prev'])): ?>
-        <a href="<?php echo get_permalink($prev_next['prev']); ?>" class="fa fa-2x fa-chevron-circle-left prev-screenshot"></a>
-      <?php endif; ?>
-      <?php if (!empty($prev_next['next'])): ?>
-        <a href="<?php echo get_permalink($prev_next['next']); ?>" class="fa fa-2x fa-chevron-circle-right next-screenshot"></a>
-      <?php endif; ?>
-      <?php
-        $class = "";
-        if ($spam == "Confirmed" || $flag_count > 10) {
-          echo '<p class="spam-shield">This post is potentially offensive or inappropriate.<br><br><a href="#">Load anyway</a></p>';
-          $class = "hide";
-        }
-      ?>
-      <div class="screenshot-image-plus-caption <?php echo $class; ?>">
-        <?php the_post_thumbnail('full'); ?>
-        <div class="screenshot-meta">
-          <span class="screenshot-caption"><?php the_title(); ?></span>
-          <div class="screenshot-actions">
-            <div class="like">
-              <a class="like-emoji-link" data-emoji="funny" href="#">
-                <img src="<?php tpldir(); ?>/assets/emojiicon-funny.svg">
-              </a>
-              <span class="count-text"><?php echo $funny_likes; ?></span>
+      <div class="screenshot-card">
+        <?php $prev_next = prevNextIds(get_the_ID(), ""); // FIXME: need to pass in $query_type here ?>
+        <?php if (!empty($prev_next['prev'])): ?>
+          <a href="<?php echo get_permalink($prev_next['prev']); ?>" class="fa fa-2x fa-chevron-circle-left prev-screenshot"></a>
+        <?php endif; ?>
+        <?php if (!empty($prev_next['next'])): ?>
+          <a href="<?php echo get_permalink($prev_next['next']); ?>" class="fa fa-2x fa-chevron-circle-right next-screenshot"></a>
+        <?php endif; ?>
+        <?php
+          $class = "";
+          if ($spam == "Confirmed" || $flag_count > 10) {
+            echo '<p class="spam-shield">This post is potentially offensive or inappropriate.<br><br><a href="#">Load anyway</a></p>';
+            $class = "hide";
+          }
+        ?>
+        <div class="screenshot-image-plus-caption <?php echo $class; ?>">
+          <?php the_post_thumbnail('full'); ?>
+          <div class="screenshot-meta">
+            <span class="screenshot-caption"><?php the_title(); ?></span>
+            <div class="screenshot-actions">
+              <div class="like">
+                <a class="like-emoji-link" data-emoji="funny" href="#">
+                  <img src="<?php tpldir(); ?>/assets/emojiicon-funny.svg">
+                </a>
+                <span class="count-text"><?php echo $funny_likes; ?></span>
 
-              <a class="like-emoji-link" data-emoji="angry" href="#">
-                <img src="<?php tpldir(); ?>/assets/emojiicon-angry.svg">
-              </a>
-              <span class="count-text"><?php echo $angry_likes; ?></span>
+                <a class="like-emoji-link" data-emoji="angry" href="#">
+                  <img src="<?php tpldir(); ?>/assets/emojiicon-angry.svg">
+                </a>
+                <span class="count-text"><?php echo $angry_likes; ?></span>
 
-              <a class="like-emoji-link" data-emoji="sad" href="#">
-                <img src="<?php tpldir(); ?>/assets/emojiicon-sad.svg">
-              </a>
-              <span class="count-text"><?php echo $sad_likes; ?></span>
-            </div>
-            <div class="share">
-              <a class="twitter-share-button"
-                 data-twitter-status="<?php echo urlencode(get_the_title() . " " . get_permalink() . " #inhumanads"); ?>"
-                 href="#">
-                <i class="fa fa-twitter-square fa-lg"></i></a>
-              <a class="facebook-share-button"
-                 data-fb-u="<?php echo urlencode(get_permalink()); ?>"
-                 data-fb-title="<?php echo urlencode(get_the_title()); ?>"
-                 href="#">
-                <i class="fa fa-facebook-square fa-lg"></i></a>
-              <a class="email-share-button"
-                 data-email="<?php echo urlencode(get_the_title() . "\n" . get_permalink()); ?>"
-                 href="#">
-                <i class="fa fa-envelope-square fa-lg"></i></a>
+                <a class="like-emoji-link" data-emoji="sad" href="#">
+                  <img src="<?php tpldir(); ?>/assets/emojiicon-sad.svg">
+                </a>
+                <span class="count-text"><?php echo $sad_likes; ?></span>
+              </div>
+              <div class="share">
+                <a class="twitter-share-button"
+                   data-twitter-status="<?php echo urlencode(get_the_title() . " " . get_permalink() . " #inhumanads"); ?>"
+                   href="#">
+                  <i class="fa fa-twitter-square fa-lg"></i></a>
+                <a class="facebook-share-button"
+                   data-fb-u="<?php echo urlencode(get_permalink()); ?>"
+                   data-fb-title="<?php echo urlencode(get_the_title()); ?>"
+                   href="#">
+                  <i class="fa fa-facebook-square fa-lg"></i></a>
+                <a class="email-share-button"
+                   data-email="<?php echo urlencode(get_the_title() . "\n" . get_permalink()); ?>"
+                   href="#">
+                  <i class="fa fa-envelope-square fa-lg"></i></a>
+              </div>
             </div>
           </div>
         </div>
-      </div>
-      <div class="screenshot-meta-2">
-        <?php if ($brand): ?>
-          <span class="screenshot-brand">Brand: <a href="/?s=<?php echo $brand; ?>"><?php echo $brand; ?></a></span>
-        <?php endif; ?>
-        <?php
-          $author_id = get_post_field('post_author', $post->ID);
-          $author = get_user_by('id', $author_id);
-        ?>
-        <span class="screenshot-domain">Spotted on: <a href="/?s=<?php echo $domain; ?>"><?php echo $domain; ?></a> by <a href="/?s=<?php echo $author->display_name; ?>"><?php echo $author->display_name; ?></a></span>
-      </div>
+        <div class="screenshot-meta-2">
+          <?php if ($brand): ?>
+            <span class="screenshot-brand">Brand: <a href="/?s=<?php echo $brand; ?>"><?php echo $brand; ?></a></span>
+          <?php endif; ?>
+          <?php
+            $author_id = get_post_field('post_author', $post->ID);
+            $author = get_user_by('id', $author_id);
+          ?>
+          <span class="screenshot-domain">Spotted on: <a href="/?s=<?php echo $domain; ?>"><?php echo $domain; ?></a> by <a href="/?s=<?php echo $author->display_name; ?>"><?php echo $author->display_name; ?></a></span>
+        </div>
 
-      <div class="secondary-actions">
-        <a id="report-link" href="#">
-          <i class="fa fa-warning"></i><span class="action-txt">Report</span>
-        </a>
-        <?php if ($isowner): ?>
-          &nbsp;
-          <a href="?edit">
-            <i class="fa fa-pencil"></i><span class="action-txt">Edit</span>
+        <div class="secondary-actions">
+          <a id="report-link" href="#">
+            <i class="fa fa-warning"></i><span class="action-txt">Report</span>
           </a>
-        <?php endif; ?>
+          <?php if ($isowner): ?>
+            &nbsp;
+            <a href="?edit">
+              <i class="fa fa-pencil"></i><span class="action-txt">Edit</span>
+            </a>
+          <?php endif; ?>
+        </div>
       </div>
-    </div>
 
-    <hr>
+      <hr>
 
-    <?php comments_template(); ?>
-  <?php endif; ?>
+      <?php comments_template(); ?>
+
+    <?php endif; ?>
+
   </div>
   <div class="right-bar"></div>
 </div>
